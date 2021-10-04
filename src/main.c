5eb44a2beaf0438d15348351e17053f3371a1b75
Mexazonic <brunnohenriq697@gmail.com>
2021-10-03 22:53:18 -0300

    Refatorando

--- src/main.c
+++ src/main.c
@@ -66,32 +66,32 @@ void sensores(setagem *set) {
 
 	printf( "Humildade = %.2f%% Temperatura = %.2f ºF\n", set->umidade, set->temperatura);
 	
-	set->SP_T = read_gpio(SP_T); // Sensor de Presença
+	set->SP_T_estado = read_gpio(SP_T); // Sensor de Presença
 	printf("Sensor de presenca Terreo: %d\n", set->SP_T);
-	set->SPo_T = read_gpio(SPo_T); // Portaria
+	set->SPo_T_estado = read_gpio(SPo_T); // Portaria
 	printf("Sensor da porta Terreo: %d\n", set->SPo_T);
 
-	set->SJ_T01 = read_gpio(SJ_T01); // Janela 1 
+	set->SJ_T01_estado = read_gpio(SJ_T01); // Janela 1 
 	printf("Sensor SJ_T01 - Térreo: %d\n", set->SJ_T01);
 
-	set->SJ_T02 = read_gpio(SJ_T02); // Janela 2
+	set->SJ_T02_estado = read_gpio(SJ_T02); // Janela 2
 	printf("Sensor SJ_T02 - Térreo: %d\n", set->SJ_T02);
 
-	set->SF_T = read_gpio(SF_T); // Fumacê	
+	set->SF_T_estado = read_gpio(SF_T); // Fumacê	
 	printf("Sensor Fumaça: %d\n", set->SF_T);
 
 	int n = 3;
 	while(n--) {
 
 		
-		set->SC_IN = read_gpio(SC_IN);
-		printf("Entrada: %d\n", set->SC_IN);
+		set->SC_IN_estado = read_gpio(SC_IN);
+		printf("Entrada: %d\n", set->SC_IN_estado);
 
-		set->SC_OUT = read_gpio(SC_OUT);		
-		printf("Sainda: %d\n", set->SC_OUT);
+		set->SC_OUT_estado = read_gpio(SC_OUT);		
+		printf("Sainda: %d\n", set->SC_OUT_estado);
 
 		// Fluxo de Pessoas
-		set->pessoas = set->pessoas + set->SC_IN - set->SC_OUT;
+		set->pessoas = set->pessoas + set->SC_IN_estado - set->SC_OUT_estado;
 		printf("Pessoas no local: %d\n", set->pessoas);
 
 		usleep(200000);
