56ada2c56d57970a8f2d4ba94ab32b9ba8455863
Mexazonic <brunnohenriq697@gmail.com>
2021-10-03 21:03:17 -0300

    Testando Referencia Struct

--- src/main.c
+++ src/main.c
@@ -4,10 +4,36 @@
 #include <dht11.h>
 #include "gpio.h"
 
+typedef struct {
+    int run;
+
+    // Humildade e Temperatura
+    float temperatura;
+    float umidade;
+
+    // Pessoas
+    int pessoas;
+    int SC_IN;
+    int SC_OUT;
+
+// Andare
+    int LS_T01;
+    int LS_T02;
+    int LC_T;
+    int AC_T;
+    int ASP;
+
+    int SP_T;
+    int SF_T;
+    int SJ_T01;
+    int SJ_T02;
+    int SPo_T;
+
+   
+} setagem;
+
 void init_setup();
 void close_connections();
-
-
 
 
 void read_sensors(setagem *set) {
