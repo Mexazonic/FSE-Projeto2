8fd38e4df0b1e90a2cba1d6e1d8f776f82fde833
Mexazonic <brunnohenriq697@gmail.com>
2021-10-03 21:26:52 -0300

    Teste

--- src/main.c
+++ src/main.c
@@ -1,36 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-#include "setagem.h"
-#include <dht11.h>
+#include "../inc/setagem.h"
 #include "gpio.h"
 
-typedef struct {
-    int run;
-
-    // Humildade e Temperatura
-    float temperatura;
-    float umidade;
-
-    // Pessoas
-    int pessoas;
-    int SC_IN;
-    int SC_OUT;
-
-// Andare
-    int LS_T01;
-    int LS_T02;
-    int LC_T;
-    int AC_T;
-    int ASP;
-
-    int SP_T;
-    int SF_T;
-    int SJ_T01;
-    int SJ_T02;
-    int SPo_T;
-
-   
-} setagem;
 
 void init_setup();
 void close_connections();
